5ce8ba5863454b1d9782f416a053878a58276810
PassionZale <whouu@qq.com>
2023-11-22 10:09:33 +0800

    scripts: redirects generator

--- scripts/redirects.ts
+++ scripts/redirects.ts
@@ -1,88 +1,103 @@
-import fs from 'node:fs/promises'
-import { dirname, resolve } from 'node:path'
-import process from 'node:process'
-import fg from 'fast-glob'
+import fs from "node:fs/promises";
+import { dirname, resolve } from "node:path";
+import process from "node:process";
+import fg from "fast-glob";
 
-const packageFiles = (await fg('*/src/package.json', {
-  onlyFiles: true,
-})).sort()
-
-const bases = (await Promise.all(
-  packageFiles.map(async (file) => {
-    const talkRoot = dirname(dirname(file))
-    const json = JSON.parse(await fs.readFile(file, 'utf-8'))
-    const pdfFile = (await fg('*.pdf', {
-      cwd: resolve(process.cwd(), talkRoot),
-      onlyFiles: true,
-    }))[0]
-    const command = json.scripts?.build
-    if (!command)
-      return
-    const base = command.match(/ --base (.*?)\s/)?.[1]
-    if (!base)
-      return
-    return {
-      dir: talkRoot,
-      base,
-      pdfFile,
-    }
-  }),
-))
-  .filter(Boolean)
-
-const redirects = bases
-  .flatMap(({ base, pdfFile, dir }) => {
-    const parts: string[] = []
-
-    if (pdfFile) {
-      parts.push(`
-[[redirects]]
-  from = "${base}pdf"
-  to = "https://github.com/passionzale/talks/blob/main/${dir}/${pdfFile}?raw=true"
-  status = 302
-
-[[redirects]]
-  from = "/${dir}/pdf"
-  to = "https://github.com/passionzale/talks/blob/main/${dir}/${pdfFile}?raw=true"
-  status = 302`)
-    }
-
-    parts.push(`
-[[redirects]]
-  from = "${base}src"
-  to = "https://github.com/passionzale/talks/tree/main/${dir}"
-  status = 302`)
-
-    parts.push(`
-[[redirects]]
-  from = "${dir}"
-  to = "https://talks.lovchun.com${base}"
-  status = 301
-
-[[redirects]]
-  from = "${base}*"
-  to = "${base}index.html"
-  status = 200`)
-
-    return parts
+const packageFiles = (
+  await fg("*/src/package.json", {
+    onlyFiles: true,
   })
-  .join('\n')
+).sort();
 
-const content = `
-[build.environment]
-  NODE_VERSION = "20"
-  PLAYWRIGHT_BROWSERS_PATH = "0"
+const bases = (
+  await Promise.all(
+    packageFiles.map(async (file) => {
+      const talkRoot = dirname(dirname(file));
+      const json = JSON.parse(await fs.readFile(file, "utf-8"));
+      const pdfFile = (
+        await fg("*.pdf", {
+          cwd: resolve(process.cwd(), talkRoot),
+          onlyFiles: true,
+        })
+      )[0];
 
-[build]
-  publish = "dist"
-  command = "pnpm run build"
+      const command = json.scripts?.build;
 
-${redirects}
+      if (!command) return;
 
-[[redirects]]
-  from = "/"
-  to = "https://github.com/passionzale/talks"
-  status = 302
-`
+      const base = command.match(/ --base (.*?)\s/)?.[1];
 
-await fs.writeFile('netlify.toml', content, 'utf-8')
+      if (!base) return;
+
+      return {
+        dir: talkRoot,
+        base,
+        pdfFile,
+      };
+    })
+  )
+).filter(Boolean);
+
+interface RedirectItem {
+  source: string;
+  destination: string;
+  statusCode: number;
+}
+
+const redirects = bases.flatMap(({ base, pdfFile, dir }) => {
+  const parts: RedirectItem[] = [];
+
+  if (pdfFile) {
+    parts.push({
+      source: `${base}pdf`,
+      destination: `https://github.com/passionzale/talks/blob/main/${dir}/${pdfFile}?raw=true`,
+      statusCode: 302,
+    });
+
+    parts.push({
+      source: `/${dir}/pdf`,
+      destination: `https://github.com/passionzale/talks/blob/main/${dir}/${pdfFile}?raw=true`,
+      statusCode: 302,
+    });
+  }
+
+  parts.push({
+    source: `${base}src`,
+    destination: `https://github.com/passionzale/talks/tree/main/${dir}`,
+    statusCode: 302,
+  });
+
+  parts.push({
+    source: `${dir}`,
+    destination: `https://talks.lovchun.com${base}`,
+    statusCode: 301,
+  });
+
+  parts.push({
+    source: `${base}(.*)`,
+    destination: `${base}index.html`,
+    statusCode: 200,
+  });
+
+  return parts;
+});
+
+const content = {
+  github: {
+    enabled: false,
+  },
+  buildCommand: "pnpm run build",
+  outputDirectory: "dist",
+  redirects: [
+    ...redirects,
+    ...[
+      {
+        source: "/",
+        destination: "https://github.com/passionzale/talks",
+        statusCode: 302,
+      },
+    ],
+  ],
+};
+
+await fs.writeFile("vercel.json", JSON.stringify(content, null, 2), "utf-8");
